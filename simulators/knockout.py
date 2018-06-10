bc3b2f96adf0ac1c4e062b81c785e948a5140dae
Nikhil <1028334+nikhilsaraf@users.noreply.github.com>
2018-06-10 15:17:25 -0700

    add stats for knockout stage

--- simulators/knockout.py
+++ simulators/knockout.py
@@ -13,6 +13,11 @@ class KnockoutSimulator:
         self._simulateStage("Finals", self.bracket.finals, self.bracket.champion)
         self.bracket.runnerUp[0] = self._findLoser(self.bracket.finals[0], self.bracket.finals[1], self.bracket.champion[0])
 
+        # add stats for finishers
+        self.bracket.champion[0].stats.champions = 1
+        self.bracket.runnerUp[0].stats.runner_up = 1
+        self.bracket.third[0].stats.third = 1
+
     def _simulateStage(self, stageName, fromList, nextList):
         print "Simulating Knockout Stage:", stageName
         for i in range(0, len(fromList), 2):
@@ -21,10 +26,23 @@ class KnockoutSimulator:
 
             matchResult = self.matchSimulator.simulateMatchWithoutDraws(teamA, teamB)
             if matchResult.goalsFor(teamA) > matchResult.goalsFor(teamB):
-                nextList[i/2] = teamA
+                winner = teamA
+                loser = teamB
             else:
-                nextList[i/2] = teamB
+                winner = teamB
+                loser = teamA
+            nextList[i/2] = winner
+
+            winner.stats.win += 1
+            winner.stats.lose += 1
+            self._recordGoals(teamA, matchResult)
+            self._recordGoals(teamB, matchResult)
+
             print "   {:>13} vs. {:13}:  {:>1} - {:1} ({})".format(teamA.name, teamB.name, matchResult.goalsFor(teamA), matchResult.goalsFor(teamB), matchResult.winnerString())
+
+    def _recordGoals(self, team, matchResult):
+        team.stats.goals_for += matchResult.goalsFor(team)
+        team.stats.goals_against += matchResult.goalsAgainst(team)
 
     def _findLoser(self, teamA, teamB, winner):
         if teamA.name == winner.name:
